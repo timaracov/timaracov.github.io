be66b76ae39c3b77f3fd1acd37b567f9d4f1a913
timaracov <neyenburgz@mail.ru>
2020-12-28 22:51:32 +0300

    Auto Summary

--- change_links.py
+++ change_links.py
@@ -1,6 +1,56 @@
-import os
+from bs4 import BeautifulSoup as Bs
+import os, re, requests, time
 
-files = os.listdir(f'{os.getcwd()}/docs')
-for index,file in enumerate(files):
-	name = "<p><a href={}> {}</a></p>".format(file,file)
-	print(name)
+def re_html(html,oldtag,newtag):
+	oldtags = oldtag.split('...')
+	index0 = html.find(oldtags[0])
+	index1 = html.find(oldtags[1])
+
+	if index0!=-1 and index1!=-1:
+		html = html.replace(html[index0:index1+len(oldtags[1])],newtag)
+	
+	return html
+
+k = 0
+
+while 1:
+	##### BOOKMARKS
+	htmllines = []
+	k+=1
+	
+	os.system("git commit -a -m 'Auto Summary'")
+	os.system("git push origin 'main'")
+
+	with open('/home/tima/Документы/GitHub/newrep/timaracov.github.io/changes.txt','w') as f:
+		f.write(str(k))
+
+	with open('/home/tima/Документы/GitHub/newrep/timaracov.github.io/static/docs/bookmark.dtb') as f:
+		links = f.readlines()
+
+	for link in links:
+		link = link.split('<----->')
+		htmllines.append("\t\t\t<p> <a href={}> {} </a> </p>\n".format(link[1],link[0]))
+
+	with open('/home/tima/Документы/GitHub/newrep/timaracov.github.io/pages/bmrk.html','r') as f:
+		htmlfile = f.read()
+		htmllines = '<div>\n'+''.join(htmllines)+'</div>'
+		htmlfile = re_html(htmlfile,'<div>...</div>',htmllines)
+
+	with open('/home/tima/Документы/GitHub/newrep/timaracov.github.io/pages/bmrk.html','w') as f:
+		f.write(htmlfile)
+
+	##### MUDIC FILES
+	music_files = os.listdir('/home/tima/Документы/GitHub/newrep/timaracov.github.io/static/music/')
+
+	for i in range(len(music_files)):
+		music_files[i] = f'\t\t\t<p><a href="/static/music/{music_files[i]}">{music_files[i]}</a></p>\n'
+
+	with open('/home/tima/Документы/GitHub/newrep/timaracov.github.io/pages/music.html','r') as f:
+		htmlfile = f.read()
+		htmllines = '<div>\n'+''.join(music_files)+'</div>'
+		htmlfile = re_html(htmlfile,'<div>...</div>',htmllines)
+
+	with open('/home/tima/Документы/GitHub/newrep/timaracov.github.io/pages/music.html','w') as f:
+		f.write(htmlfile)
+
+	time.sleep(50)
